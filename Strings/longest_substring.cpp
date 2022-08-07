3e1b3754e4cf0968020f712bdc3b0a23b4582429
Rishik Kumar <rishik.k2002@gmail.com>
2022-08-07 15:04:11 +0530

    initial commit

---
+++ Strings/longest_substring.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector <char> e;
+        int count = 0;
+        int max_count = 0;
+        int j=0;
+        for(int i=0; i< s.length() - max_count;i++)
+        {
+            if(e.empty())
+               { e.push_back(s[i]);
+                count++;}
+            else if(std::count(e.begin(), e.end(), s[i]))
+            {
+                if(count>max_count)
+                    max_count = count;
+                count=0;
+                e.clear();
+                i = ++j;
+            }
+            else
+            {
+                e.push_back(s[i]);
+                count++;
+            }
+        }
+        return max_count;
+    }
+};
+
+int main()
+{
+    Solution obj;
+    string s;
+    cin>>s;
+    cout<<obj.lengthOfLongestSubstring(s);
+    return 0;
+}
