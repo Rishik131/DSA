a83191ea325f287ad1eb632e0980a509ab91198b
Rishik Kumar <rishik.k2002@gmail.com>
2022-09-03 17:53:35 +0530

    basic

---
+++ Algorithms/bfs.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//take input
+int get_no_of_nodes()
+{
+    cout<<"enter no. of nodes";
+    int a;
+    cin>>a;
+    return a;
+}
+
+vector<vector<int> > make_graph(int nodes)
+{
+    return 
+}
+
+int main()
+{
+    int nodes = get_no_of_nodes();
+    vector<vector<int> > graph = make_graph(nodes);
+    return 0;
+}
