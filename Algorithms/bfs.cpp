b47e7fe7dfb1031dd5e73a7e25df0066cbd02d64
Rishik Kumar <rishik.k2002@gmail.com>
2022-09-03 18:07:21 +0530

    edges

--- Algorithms/bfs.cpp
+++ Algorithms/bfs.cpp
@@ -10,14 +10,32 @@ int get_no_of_nodes()
     return a;
 }
 
+//graph generator
 vector<vector<int> > make_graph(int nodes)
 {
-    return 
+    vector<vector<int> > graph(nodes);
+    return (graph);
+}
+
+//adding edges
+void add_edge(vector<vector<int> > &graph)
+{
+    cout<<"Enter no. of edges";
+    int a;
+    cin>>a;
+    for (int i = 0; i < a; i++)
+    {
+        int source,destination;
+        cin>>source>>destination;
+        
+    }
+    
 }
 
 int main()
 {
     int nodes = get_no_of_nodes();
     vector<vector<int> > graph = make_graph(nodes);
+    add_edge(graph);
     return 0;
 }
